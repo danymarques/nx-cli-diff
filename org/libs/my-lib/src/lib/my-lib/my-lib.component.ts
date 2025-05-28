eb178d0bba49a2810fef560dd9585b59863677eb
danymarques <dany.marques90@gmail.com>
2025-05-28 13:29:23 +0000

    chore: version 20.2.0

--- org/libs/my-lib/src/lib/my-lib/my-lib.component.ts
+++ org/libs/my-lib/src/lib/my-lib/my-lib.component.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'lib-my-lib',
-  standalone: true,
   imports: [CommonModule],
   templateUrl: './my-lib.component.html',
   styleUrl: './my-lib.component.css',
